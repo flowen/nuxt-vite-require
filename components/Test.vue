bc03ce33f300ccea6368145546d5d20074616479
Rou Hun <lowen.f@gmail.com>
2021-03-01 09:35:34 +0000

    first commit

---
+++ components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    test
+
+    <img :src="image" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    image: {
+      type: String,
+      default: null,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
